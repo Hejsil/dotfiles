8b8eb63102d925ee3f3e9b1028e5e0541030bd1a
Jimmi HC <jhc@liab.dk>
2018-06-27 15:01:35 +0200

    Added more tools to setup.sh

--- setup.sh
+++ setup.sh
@@ -3,21 +3,33 @@
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
-mkdir build
-cd build
-sudo pacman -S go
-curl -o PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=yay
-makepkg -i PKGBUILD --noconfirm
-cd ..
-rm -r build
+(
+    cd "$(mktemp -d)" || exit 1
+    sudo pacman -S --noconfirm go
+    curl -o PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=yay
+    makepkg -i PKGBUILD --noconfirm
+    rm -r "$(pwd)"
+)
 
-yay -S yay
-yay -S gnu-netcat
-yay -S zsh
-yay -S oh-my-zsh-git
-yay -S zsh-pure-prompt
-yay -S zsh-fast-syntax-highlighting-git
-yay -S zsh-autosuggestions
+yay -S --noconfirm yay
+
+# zsh stuff
+yay -S --noconfirm zsh
+yay -S --noconfirm oh-my-zsh-git
+yay -S --noconfirm zsh-pure-prompt
+yay -S --noconfirm zsh-fast-syntax-highlighting-git
+yay -S --noconfirm zsh-autosuggestions
+
+# Term tools
+yay -S --noconfirm gnu-netcat
+yay -S --noconfirm skim
+yay -S --noconfirm ripgrep
+yay -S --noconfirm fd
+yay -S --noconfirm hex
+yay -S --noconfirm thefuck
+
+# Gui tools
+yay -S --noconfirm visual-studio-code-bin
 
 chsh -s /usr/bin/zsh
 
