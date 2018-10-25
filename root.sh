8a79d6c7995c3540aecf07fd451c217d0a3da598
Jimmi HC <jimmiholstchristensen@gmail.com>
2018-10-25 21:52:52 +0200

    Disable overcommitting

--- root.sh
+++ root.sh
@@ -8,3 +8,8 @@ systemctl enable "dhcpcd@$INTERFACE.service"
 
 pacman -S sudo
 sed -i 's/# ALL/ALL/' /etc/sudoers
+
+# Disable overcommit, because I really don't want it to
+# stall my system when someone hogs all the memory.
+echo "vm.overcommit_memory = 2" >> /etc/sysctl.conf
+echo "vm.overcommit_kbytes = 0" >> /etc/sysctl.conf
