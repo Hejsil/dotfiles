6e3b70b413e50bc8e21afeb8520b1e3154559f73
Jimmi Holst Christensen <jimmi@ziglang.org>
2018-12-23 18:10:41 +0100

    Add mail and name to git config in github script

--- github-ssh.sh
+++ github-ssh.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-ssh-keygen -t rsa -b 4096 -C "jimmiholstchristensen@gmail.com"
+git config --global user.email "jimmi@ziglang.org"
+git config --global user.name "Jimmi Holst Christensen"
+ssh-keygen -t rsa -b 4096 -C "jimmi@ziglang.org"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
 xclip -sel clip < ~/.ssh/id_rsa.pub
