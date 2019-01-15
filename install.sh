b230c8fea20551559d0c9587011b9e8aa279ae6e
Jimmi HC <jimmiholstchristensen@gmail.com>
2019-01-15 20:01:54 +0100

    Just new newlines

--- install.sh
+++ install.sh
@@ -2,7 +2,10 @@
 HERE="$(dirname "$(realpath "$0")")"
 cd "$HERE"
 
-for FILE in $(find -type f -not -path "./.git/*" -not -path "./install.sh"); do
+for FILE in $(find -type f\
+    -not -path './.git/*'\
+    -not -path './install.sh'\
+    ); do
     mkdir -p "$HOME/$(dirname "$FILE")"
     ln -snf "$HERE/$FILE" "$HOME/$FILE"
 done
