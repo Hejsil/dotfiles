c5631a5e56a843c676002cf26c5b745c6aee4c8a
Jimmi Holst Christensen <jhc@dismail.de>
2019-08-18 20:33:03 +0200

    kill lemonbar for real on reload

--- .local/script/bar.sh
+++ .local/script/bar.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
+trap 'trap - TERM; kill 0' INT TERM QUIT EXIT
+
 # Load theme from .Xresources
 eval "$(grep 'color[0-9]*:[ ]*#' "$HOME/.Xresources" | sed -E 's/^\*color([0-9]*):[ ]*(.*)$/COLOR\1="\2"/')"
 
+BAR_HEIGHT="35"
+bspc config top_padding "$BAR_HEIGHT"
+
 bar-info.sh | bar-mk.sh | lemonbar  \
+    -g "x$BAR_HEIGHT"               \
     -f 'monospace:size=17'          \
     -B "$COLOR0"                    \
     -F "$COLOR15"                   \
     -u '2'                          \
-    -o '1'
+    -o '1' &
+
+wait
--- .local/script/runonce.sh
+++ .local/script/runonce.sh
@@ -4,7 +4,7 @@ STDIN="$(if [ -t 0 ]; then echo ""; else cat; fi)"
 DIR="/tmp/processes"
 FILE="$DIR/$1"
 mkdir -p "$DIR"
-[ -e "$FILE" ] && return
+[ -e "$FILE" ] && exit 0
 
 echo "$STDIN" | "$@" &
 echo "$!" >"$FILE"
