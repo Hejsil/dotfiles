6f359627a4ee63bb2e711101d44878ae54caf4e4
Jimmi Holst Christensen <jhc@dismail.de>
2019-08-18 22:57:00 +0200

    unicodes now downloads to a tmp file to reuse

--- .local/script/unicodes.sh
+++ .local/script/unicodes.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-wget -O - https://www.unicode.org/Public/UCD/latest/ucd/UnicodeData.txt | \
-    while IFS=';' read -r CODE DESC REST; do echo '\u'"$CODE 0x$CODE $DESC"; done \
+
+UNICODE="/tmp/unicode.txt"
+[ -e "$UNICODE" ] || curl https://www.unicode.org/Public/UCD/latest/ucd/UnicodeData.txt >"$UNICODE"
+
+while IFS=';' read -r CODE DESC REST; do echo '\u'"$CODE 0x$CODE $DESC"; done < "$UNICODE" \
     | ascii2uni -a U -q | rofi -dmenu -i | cut -d ' ' -f 1 | xclip -sel clip
