5a37f56eff6de61c9816f9173d3dfaafc0d8d7e8
Jimmi Holst Christensen <jhc@dismail.de>
2019-08-19 19:55:29 +0200

    runonce.sh also check if PID is running

--- .local/script/runonce.sh
+++ .local/script/runonce.sh
@@ -4,7 +4,7 @@ STDIN="$(if [ -t 0 ]; then echo ""; else cat; fi)"
 DIR="/tmp/processes"
 FILE="$DIR/$1"
 mkdir -p "$DIR"
-[ -e "$FILE" ] && exit 0
+[ -e "$FILE" ] && kill -0 "$(cat "$FILE")" && exit 0
 
 echo "$STDIN" | "$@" &
 echo "$!" >"$FILE"
