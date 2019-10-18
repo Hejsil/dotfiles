6e98e718fe7b2d3754ea43e12b47f1ac5d4cfcf0
Jimmi Holst Christensen <jhc@dismail.de>
2019-10-18 17:42:10 +0200

    open.sh now logs, and we execute deluge+java

--- .local/script/open.sh
+++ .local/script/open.sh
@@ -10,6 +10,8 @@ mime_match() {
     file -i "$ARG" | grep -E "$1"
 }
 
+echo "$ARG" >> /tmp/open.log
+
 if mime_match "application/pdf;"; then
     "$READER" "$ARG"
 elif mime_match "text/.*;|application/json"; then
@@ -18,6 +20,10 @@ elif mime_match "image/.*;"; then
     sxiv "$ARG"
 elif mime_match "(audio|video)/.*;"; then
     mpv "$ARG"
+elif mime_match "application/java-archive;"; then
+    java -jar "$ARG"
+elif mime_match "application/x-bittorrent;"; then
+    deluge "$ARG"
 elif name_match "^(https?:\/\/)?(www\.)?youtu(\.be\/.*|be\.com\/watch\?.*v=.*)$"; then
     mpv "$ARG"
 elif name_match "^(https?:\/\/)|(www\.)"; then
