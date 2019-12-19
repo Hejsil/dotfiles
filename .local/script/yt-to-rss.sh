0da21aaa6c79ac634170c83d2acc9986bf962b31
Jimmi Holst Christensen <jhc@dismail.de>
2019-12-19 22:45:25 +0100

    yt-to-rss now downloads from yt api

--- .local/script/yt-to-rss.sh
+++ .local/script/yt-to-rss.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-grep 'channel-info' | cut -d'"' -f6 | cut -d"/" -f3 | sed -E 's#(.*)#https://www.youtube.com/feeds/videos.xml?channel_id=\1#g' | sort
+curl 'https://www.googleapis.com/youtube/v3/subscriptions?pageToken=CDIQAQ&part=snippet%2CcontentDetails&channelId=UCch55ju1FlFdcDGQuMdRnxg&maxResults=50&key=AIzaSyBu8YAifpfHVbBR2wSbZ5a0Dp6WGEshy88' |
+    jq -r '.items[].snippet.resourceId.channelId' |
+    sed 's#^#https://www.youtube.com/feeds/videos.xml?channel_id=#'
