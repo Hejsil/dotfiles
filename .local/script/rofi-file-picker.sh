39a0c934f1fe0f938f7898dcecb93777e0e9b458
Jimmi Holst Christensen <jhc@dismail.de>
2019-12-19 22:52:28 +0100

    Custom quick open for textadept
    and other minor changes

---
+++ .local/script/rofi-file-picker.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+PROGRAM="$(basename "$0")"
+usage() {
+    echo "Usage: $(basename "$PROGRAM")"
+}
+
+while getopts "h" OPT; do
+    case "$OPT" in
+        h)
+            usage
+            exit 0
+            ;;
+        *)
+            usage
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+DIR="$1"; [ -z "$DIR" ] && { echo "Missing DIR"; exit 1; }
+fd '.' "$DIR" -H -t f | head -n 50000 | rofi -dmenu
