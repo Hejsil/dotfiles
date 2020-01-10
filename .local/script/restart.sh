f4ddbd6307739fd1df90655f5b33f759e44ddb10
Jimmi Holst Christensen <jhc@dismail.de>
2020-01-10 20:24:13 +0100

    Fix restart.sh and runonce.sh

--- .local/script/restart.sh
+++ .local/script/restart.sh
@@ -2,7 +2,7 @@
 STDIN="$(if [ -t 0 ]; then echo ""; else cat; fi)"
 
 DIR="/tmp/processes"
-FILE="$DIR/$1"
+FILE="$DIR/$(echo "$@" | sed 's#/#|#g')"
 mkdir -p "$DIR"
 [ -e "$FILE" ] && kill "$(cat "$FILE")"
 
