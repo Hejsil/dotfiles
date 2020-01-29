05beeb1586b765c97e4eef5f9ec49505565a498b
Jimmi Holst Christensen <jhc@dismail.de>
2020-01-29 21:27:26 +0100

    Screenshot exit of maim fails

--- .local/script/screenshot.sh
+++ .local/script/screenshot.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 FILE="$1/$(date '+%y-%m-%dT%H:%M:%S').png"
-maim -s > "$FILE"
+maim -s "$FILE" || exit 1
 xclip -selection clipboard -t image/png -i "$FILE"
 notify-send 'Screenshot Taken' "$FILE"
