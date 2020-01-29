ee91ecad1fb542ccea4b6be505b78f8f05717bcf
Jimmi Holst Christensen <jhc@dismail.de>
2020-01-29 22:33:01 +0100

    Rewrote wall.sh

--- .local/script/wall.sh
+++ .local/script/wall.sh
@@ -1,6 +1,39 @@
 #!/bin/sh
 
+PROGRAM="$(basename "$0")"
+usage() {
+    echo "Usage: $(basename "$PROGRAM") [ -t TIME ] [ -m MONITORS ] FOLDER"
+    exit "$1"
+}
+
+MONITORS=""
+TIME="1m"
+while getopts "ht:m:" OPT; do
+    case "$OPT" in
+        t) TIME="$OPTARG" ;;
+        m) MONITORS="$OPTARG" ;;
+        h) usage 0 ;;
+        *) usage 1 ;;
+    esac
+done
+shift $((OPTIND-1))
+
+FOLDER="$1"; [ -z "$FOLDER" ] && { echo "No folder provided"; exit 1; }
+[ -z "$MONITORS" ] && MONITORS="$(xrandr | grep ' connected' | wc -l)"
+[ -z "$MONITORS" ] && { echo "No amount of monitors specified"; exit 1; }
+
+# TODO: Actually use MONITORS variable
+
+TMP="$(mktemp)"
+
 while true; do
-    feh --randomize --bg-fill ~/MEGA/Pictures/Wallpapers
-    sleep 1m
+    find "$FOLDER" -type f >"$TMP" || exit 1
+    sort -R "$TMP"
+done | paste - - | while IFS="$(printf '\t')" read -r FIRST SECOND; do
+    if [ "$FIRST" = "$SECOND" ]; then
+        continue
+    fi
+
+    feh --no-fehbg --bg-fill "$FIRST" "$SECOND"
+    sleep "$TIME"
 done
