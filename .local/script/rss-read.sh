a83dafbe8dc1de297a277a575227cc15c7bec09e
Jimmi Holst Christensen <jhc@dismail.de>
2020-02-26 17:05:53 +0100

    rss-read.sh now also shows channel names

--- .local/script/rss-read.sh
+++ .local/script/rss-read.sh
@@ -18,13 +18,13 @@ done
 TMP=$(mktemp)
 TAB=$(printf '\t')
 rss-list.sh -u |
-    cut -d"$TAB" -f1,3,4 |
-    awk -F"$TAB" '{ printf "%s\t%s\t%s\n", $2, $3, $1 }' |
+    cut -d"$TAB" -f1,3,4,7 |
+    awk -F"$TAB" '{ printf "%s\t%s\t%s\t%s\n", $2, $4, $3, $1 }' |
     rofi -dmenu >"$TMP"
 
 if [ -s "$TMP" ]; then
-    cut -d"$TAB" -f2 < "$TMP" | xargs open.sh
-    cut -d"$TAB" -f3 < "$TMP" | xargs -I{} mv {} "$HOME/.cache/rss/read"
+    cut -d"$TAB" -f3 < "$TMP" | xargs open.sh
+    cut -d"$TAB" -f4 < "$TMP" | xargs -I{} mv {} "$HOME/.cache/rss/read"
 fi
 
 rm "$TMP"
