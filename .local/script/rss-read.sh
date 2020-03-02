f7ad23fd11236cc27a932b58f52daa4e37bc9e44
Jimmi Holst Christensen <jhc@dismail.de>
2020-03-02 17:34:48 +0100

    rss-read aligns text

--- .local/script/rss-read.sh
+++ .local/script/rss-read.sh
@@ -19,12 +19,12 @@ TMP=$(mktemp)
 TAB=$(printf '\t')
 rss-list.sh -u |
     cut -d"$TAB" -f1,3,4,7 |
-    awk -F"$TAB" '{ printf "%s\t%s\t%s\t%s\n", $2, $4, $3, $1 }' |
+    awk -F"$TAB" '{ printf "%-60s\t%-20s\t%s\t%s\n", substr($2, 0, 60), substr($4, 0, 20), $3, $1 }' |
     rofi -dmenu >"$TMP"
 
 if [ -s "$TMP" ]; then
-    cut -d"$TAB" -f3 < "$TMP" | xargs open.sh
-    cut -d"$TAB" -f4 < "$TMP" | xargs -I{} mv {} "$HOME/.cache/rss/read"
+    cut -d"$TAB" -f3 "$TMP" | xargs open.sh
+    cut -d"$TAB" -f4 "$TMP" | xargs -I{} mv {} "$HOME/.cache/rss/read"
 fi
 
 rm "$TMP"
