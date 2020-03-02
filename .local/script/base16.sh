f701341a4d676751016955994292e6e0dac6bd5c
Jimmi Holst Christensen <jhc@dismail.de>
2020-03-02 18:49:41 +0100

    base16.sh is a lot cleaner

--- .local/script/base16.sh
+++ .local/script/base16.sh
@@ -15,48 +15,21 @@ if [ -z "$TEMPLATE" ]; then
     echo "$TEMPLATE_CONTENT" >"$TEMPLATE"
 fi
 
-eval "$(cat "$SCHEME")"
+eval "$(cat )"
 
 # Very ugly, but fast way to generate sed arguments that replace
 # the template replacements with colors from the scheme.
 # Find a prettier way that is just as fast.
-seq 0 15 | while read -r NUM; do {
-    COLORVAR="\$COLOR$NUM"
-    HEX="$(eval "echo \"$COLORVAR\"")"
-    echo "$HEX" | fold -w2 | paste -sd ' ' - | {
-    read -r HEX_R HEX_G HEX_B
-
-    printf '%d %d %d' "0x$HEX_R" "0x$HEX_G" "0x$HEX_B" | {
-    read -r RGB_R RGB_G RGB_B
-
-    {
-        echo "$RGB_R / 255"
-        echo "$RGB_G / 255"
-        echo "$RGB_B / 255"
-    } | bc -l | paste -sd ' ' - | {
-    read -r DEC_R DEC_G DEC_B
-    
-    printf ' -e s/{{base0%X-hex}}/%s/g 
- -e s/{{base0%X-hex-r}}/%s/g
- -e s/{{base0%X-hex-g}}/%s/g
- -e s/{{base0%X-hex-b}}/%s/g
- -e s/{{base0%X-hex-bgr}}/%s/g
- -e s/{{base0%X-rgb-r}}/%s/g
- -e s/{{base0%X-rgb-g}}/%s/g
- -e s/{{base0%X-rgb-b}}/%s/g
- -e s/{{base0%X-dec-r}}/0%s/g
- -e s/{{base0%X-dec-g}}/0%s/g
- -e s/{{base0%X-dec-b}}/0%s/g' \
-    "$NUM" "$HEX" \
-    "$NUM" "$HEX_R" \
-    "$NUM" "$HEX_G" \
-    "$NUM" "$HEX_B" \
-    "$NUM" "${HEX_B}${HEX_G}${HEX_R}" \
-    "$NUM" "$RGB_R" \
-    "$NUM" "$RGB_G" \
-    "$NUM" "$RGB_B" \
-    "$NUM" "$DEC_R" \
-    "$NUM" "$DEC_G" \
-    "$NUM" "$DEC_B"
-    } } } } &
-done | xargs sed "$TEMPLATE"
+<"$SCHEME" sed -e '/^#/d' -e 's/^COLOR//' | awk --non-decimal-data -F'=' '{
+    R = substr($2, 0, 2)
+    G = substr($2, 2, 2)
+    B = substr($2, 4, 2)
+    printf " -e s/{{base0%X-hex}}/%s/g", $1, $2
+    printf " -e s/{{base0%X-hex-r}}/%s/g", $1, R
+    printf " -e s/{{base0%X-hex-g}}/%s/g", $1, G
+    printf " -e s/{{base0%X-hex-b}}/%s/g", $1, B
+    printf " -e s/{{base0%X-hex-bgr}}/%s%s%s/g", $1, G, B, R
+    printf " -e s/{{base0%X-rgb-r}}/%d/g", $1, "0x" R
+    printf " -e s/{{base0%X-rgb-g}}/%d/g", $1, "0x" G
+    printf " -e s/{{base0%X-rgb-b}}/%d/g", $1, "0x" B
+}' | xargs sed "$TEMPLATE"
