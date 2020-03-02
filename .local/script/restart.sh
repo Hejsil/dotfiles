f02f10b53f3e1682fb012e60abbc8af9a1a0b244
Jimmi Holst Christensen <jhc@dismail.de>
2020-03-02 18:19:11 +0100

    Start one off programs in .xinitrc
    runonce.sh doesn't seem to work properly in
    bspwmrc.

--- .local/script/restart.sh
+++ .local/script/restart.sh
@@ -2,9 +2,11 @@
 STDIN=$(if [ -t 0 ]; then echo ''; else cat; fi)
 
 DIR='/tmp/processes'
+LOG_DIR='/tmp/logs'
 FILE="$DIR/$(echo "$@" | sed 's#/#|#g')"
-mkdir -p "$DIR"
+LOG_FILE="$LOG_DIR/$(echo "$@" | sed 's#/#|#g')"
+mkdir -p "$DIR" "$LOG_DIR"
 [ -s "$FILE" ] && kill -- "-$(cat "$FILE")"
 
-echo "$STDIN" | "$@" &
+echo "$STDIN" | nohup "$@" 2>"$LOG_FILE" >"$LOG_FILE" &
 ps -o pgid= "$!" >"$FILE"
--- .local/script/runonce.sh
+++ .local/script/runonce.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 STDIN=$(if [ -t 0 ]; then echo; else cat; fi)
+echo "$@" >> /tmp/runonce.log
 
 DIR='/tmp/processes'
+LOG_DIR='/tmp/logs'
 FILE="$DIR/$(echo "$@" | sed 's#/#|#g')"
-mkdir -p "$DIR"
+LOG_FILE="$LOG_DIR/$(echo "$@" | sed 's#/#|#g')"
+mkdir -p "$DIR" "$LOG_DIR"
+
 [ -s "$FILE" ] && [ -e "/proc/$(cat "$FILE")" ] && exit 0
 
-echo "$STDIN" | "$@" &
+echo "$STDIN" | nohup "$@" 2>"$LOG_FILE" >"$LOG_FILE" &
 echo "$!" >"$FILE"
