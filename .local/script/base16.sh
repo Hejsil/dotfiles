f79bef48a93abdf236ffdb2934dab0ce7d7d9b42
Jimmi Holst Christensen <jhc@dismail.de>
2020-03-02 19:12:10 +0100

    Fix broken substr logic in base16
    Urg, indexing starting at 1...

--- .local/script/base16.sh
+++ .local/script/base16.sh
@@ -19,14 +19,14 @@ fi
 # the template replacements with colors from the scheme.
 # Find a prettier way that is just as fast.
 <"$SCHEME" sed -e '/^#/d' -e 's/^COLOR//' | awk --non-decimal-data -F'=' '{
-    R = substr($2, 0, 2)
-    G = substr($2, 2, 2)
-    B = substr($2, 4, 2)
+    R = substr($2, 1, 2)
+    G = substr($2, 3, 2)
+    B = substr($2, 5, 2)
     printf " -e s/{{base0%X-hex}}/%s/g", $1, $2
     printf " -e s/{{base0%X-hex-r}}/%s/g", $1, R
     printf " -e s/{{base0%X-hex-g}}/%s/g", $1, G
     printf " -e s/{{base0%X-hex-b}}/%s/g", $1, B
-    printf " -e s/{{base0%X-hex-bgr}}/%s%s%s/g", $1, G, B, R
+    printf " -e s/{{base0%X-hex-bgr}}/%s%s%s/g", $1, B, G, R
     printf " -e s/{{base0%X-rgb-r}}/%d/g", $1, "0x" R
     printf " -e s/{{base0%X-rgb-g}}/%d/g", $1, "0x" G
     printf " -e s/{{base0%X-rgb-b}}/%d/g", $1, "0x" B
