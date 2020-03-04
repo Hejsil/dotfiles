66f211efd0ae12d14842c71ce809899656d70a10
Jimmi Holst Christensen <jhc@dismail.de>
2020-03-04 21:22:37 +0100

    A little more warning on the go-to-sleep script

--- .local/script/go-to-sleep.sh
+++ .local/script/go-to-sleep.sh
@@ -19,7 +19,13 @@ while true; do
     case $(date '+%u:%H:%M') in
         [1-4,7]:23:5*)
             notify-send -u 'critical' 'Go to sleep' 'Power off in 30m'
-            sleep 30m
+            sleep 15m
+            notify-send -u 'critical' 'Go to sleep' 'Power off in 15m'
+            sleep 10m
+            notify-send -u 'critical' 'Go to sleep' 'Power off in 5m'
+            sleep 4m
+            notify-send -u 'critical' 'Go to sleep' 'Power off in 1m'
+            sleep 1m
             poweroff
             ;;
         *) ;;
