4ad7b378de388e7d744266904231c37d187afacc
Jimmi Holst Christensen <jhc@dismail.de>
2020-03-23 18:08:03 +0100

    Simplify volume setup

--- .local/script/mute.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-SET=$1
-if [ -z "$SET" ]; then
-    echo 'No input' >&2
-    exit 1
-fi
-
-sinks.sh | xargs -I{} pactl set-sink-mute {} "$1"
-touch /tmp/volume-notify-file
--- .local/script/sinks.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-pactl list sinks | grep Sink | sed 's/Sink #//'
