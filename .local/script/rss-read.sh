20a9051ff644decb32f173808f64b63406adf16c
Jimmi Holst Christensen <jhc@dismail.de>
2020-03-27 23:29:38 +0100

    Rofi and dmenu no more. Use fzf+st instead

--- .local/script/rss-read.sh
+++ .local/script/rss-read.sh
@@ -20,10 +20,10 @@ tab=$(printf '\t')
 rss-list.sh -u |
     cut -d"$tab" -f1,3,4,7 |
     awk -F"$tab" '{ printf "%-60s\t%-20s\t%s\t%s\n", substr($2, 0, 60), substr($4, 0, 20), $3, $1 }' |
-    rofi -dmenu >"$tmp"
+    fzf >"$tmp"
 
 if [ -s "$tmp" ]; then
-    cut -d"$tab" -f3 "$tmp" | xargs open.sh
+    cut -d"$tab" -f3 "$tmp" | xargs daemonize.sh open.sh
     cut -d"$tab" -f4 "$tmp" | xargs -I{} mv {} "$HOME/.cache/rss/read"
 fi
 
