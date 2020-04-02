ebf77be02a1f0faa04e4e8d7dbe036ba2cac5d0f
Jimmi Holst Christensen <jhc@dismail.de>
2020-04-02 19:05:57 +0200

    Add README

--- install.sh
+++ install.sh
@@ -2,7 +2,12 @@
 here="$(dirname "$(realpath "$0")")"
 cd "$here" || exit 1
 
-find . -type f -not -path './.git/*' -not -path './install.sh' | while read -r file; do
+find . -type f \
+    -not -path './.git/*' \
+    -not -path './install.sh' \
+    -not -path './image.png' \
+    -not -path './README.md' |
+while read -r file; do
     mkdir -p "$HOME/$(dirname "$file")"
     ln -snf "$here/$file" "$HOME/$file"
 done
