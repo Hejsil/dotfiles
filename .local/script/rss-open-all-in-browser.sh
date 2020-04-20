66da98f9a11fe1e9a9da7760c0381aa6a78beb17
Jimmi Holst Christensen <jhc@dismail.de>
2020-04-20 18:04:22 +0200

    Use enclosure in rss when there

--- .local/script/rss-open-all-in-browser.sh
+++ .local/script/rss-open-all-in-browser.sh
@@ -17,15 +17,14 @@ done
 
 tmp=$(mktemp)
 tab=$(printf '\t')
-rss-list.sh -u | cut -d"$tab" -f1,4 |
-while IFS=$tab read -r FILE LINK; do
-    printf '%s\t%s\t%s\n' "$(opener.sh "$LINK")" "$FILE" "$LINK"
-done | grep "^$BROWSER" | cut -d"$tab" -f2,3 | head -n 50 >"$tmp"
+rss-list.sh -u | cut -f1,4,8 |
+while IFS=$tab read -r file link enclosure; do
+    printf '%s\t%s\t%s\t%s\n' "$(opener.sh "$link")" "$file" "$link" "$enclosure"
+done | grep "^$BROWSER" | cut -f2,3,4 | head -n 50 >"$tmp"
 
 if [ -s "$tmp" ]; then
-    cut -d"$tab" -f2 < "$tmp" | tr '\n' ' ' | xargs "$BROWSER" &
-    cut -d"$tab" -f1 < "$tmp" | xargs -I{} mv {} "$HOME/.cache/rss/read" &
-    wait
+    cut -f2,3 "$tmp" | sed 's/\t$//' | rev | cut -f1 | rev | tr '\n' ' ' | xargs daemonize.sh "$BROWSER"
+    cut -f1 "$tmp" | xargs -I{} mv {} "$HOME/.cache/rss/read"
 fi
 
 rm "$tmp"
