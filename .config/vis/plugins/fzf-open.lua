4b8ac665030d28f1cbf6b5eaceed9f37661df11f
Jimmi Holst Christensen <jhc@dismail.de>
2020-05-14 17:58:42 +0200

    rg-open plugin for vis

--- .config/vis/plugins/fzf-open.lua
+++ .config/vis/plugins/fzf-open.lua
@@ -13,7 +13,7 @@
 -- You should have received a copy of the GNU Affero General Public License
 -- along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-module = {}
+local module = {}
 module.fzf_path = "fzf"
 module.fzf_args = ""
 
--- .config/vis/visrc.lua
+++ .config/vis/visrc.lua
@@ -1,7 +1,8 @@
 -- load standard vis module, providing parts of the Lua API
 require('vis')
-require('plugins/zig')
-require('plugins/fzf-open')
+local zig = require('plugins/zig')
+local rg_open = require('plugins/rg-open')
+local fzf_open = require('plugins/fzf-open')
 
 vis.ftdetect.filetypes.zig = {
     ext = { "%.zig$" },
@@ -34,5 +35,7 @@ vis.events.subscribe(vis.events.WIN_OPEN, function(win)
     vis:map(vis.modes.NORMAL, '<C-Right>', 'w')
     vis:map(vis.modes.NORMAL, '<C-p>', function() vis:command('fzf') end)    
 
-
+    vis:map(vis.modes.INSERT, '<C-Up>', function () vis:feedkeys('<vis-selection-new-lines-above-first>') end)
+    vis:map(vis.modes.INSERT, '<C-Down>', function () vis:feedkeys('<vis-selection-new-lines-below-last>') end)
+    vis:map(vis.modes.INSERT, '<C-p>', function() vis:command('fzf') end)    
 end)
