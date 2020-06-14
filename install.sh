31f5a286f9a5fd817c14b1b91111062f62da59cb
Jimmi Holst Christensen <jhc@dismail.de>
2020-06-15 00:47:16 +0200

    New simpler install script

--- install.sh
+++ install.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
-here="$(dirname "$(realpath "$0")")"
-cd "$here" || exit 1
-
-find . -type f \
-    -not -path './.git/*' \
-    -not -path './install.sh' \
-    -not -path './image.png' \
-    -not -path './README.md' |
-while read -r file; do
-    mkdir -p "$HOME/$(dirname "$file")"
-    ln -snf "$here/$file" "$HOME/$file"
-done
+ln -snf "$(pwd)/.bash_profile" "$HOME/.bash_profile"
+ln -snf "$(pwd)/.bashrc" "$HOME/.bashrc"
+ln -snf "$(pwd)/.config" "$HOME/.config"
+ln -snf "$(pwd)/.gitignore" "$HOME/.gitignore"
+ln -snf "$(pwd)/.ignore" "$HOME/.ignore"
+ln -snf "$(pwd)/.local/script" "$HOME/.local/script"
+ln -snf "$(pwd)/.profile" "$HOME/.profile"
+ln -snf "$(pwd)/.xinitrc" "$HOME/.xinitrc"
