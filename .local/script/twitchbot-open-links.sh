0fcb085a047df46c3b3682fbcad2515aebf2683c
Jimmi Holst Christensen <jhc@dismail.de>
2020-08-21 01:24:42 +0200

    Twitch botting

---
+++ .local/script/twitchbot-open-links.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+channel=$1
+browser=$2
+chat_dir=$3
+chat_file="$chat_dir/$channel"
+
+mkdir -p "$chat_dir"
+touch "$chat_file"
+
+tail -n 0 -f "$chat_file" |
+    rg --line-buffered -o -v "^($channel\.)?tmi.twitch.tv:" |
+    rg --line-buffered -o '(https?://)?(www\.)?(\w*\.)+(\w+)(/[^\s]*)?' |
+    xargs -d'\n' -n 1 $browser
+
