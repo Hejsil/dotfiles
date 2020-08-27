b30c9eac329da8c121e36400304ebb0d4b9e23c4
Jimmi Holst Christensen <jhc@dismail.de>
2020-08-27 02:15:20 +0200

    script for adding yt to rss urls

---
+++ .local/script/add-yt-to-rss.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+channel_id=$1
+channel_name=$2
+
+printf "\nhttps://www.youtube.com/feeds/videos.xml?channel_id=%s\t%s\n" "$channel_id" "$channel_name" >> "$HOME/.config/rss/urls"
+sort -u "$HOME/.config/rss/urls" | sponge "$HOME/.config/rss/urls"
+
