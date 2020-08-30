c413707b41cca9e61881397b5fe5ff2320e2bafc
Jimmi Holst Christensen <jhc@dismail.de>
2020-08-30 23:21:09 +0200

    Steam game launcher

---
+++ .local/script/launch-program.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+setsid -f $(path-exes.sh | fzf --print-query | grep -v '^$' | tail -n 1)
--- .local/script/rss-read.sh
+++ .local/script/rss-read.sh
@@ -19,8 +19,8 @@ tmp=$(mktemp)
 tab=$(printf '\t')
 rss-list.sh -u |
     cut -f1,3,4,7,8 |
-    awk -F"$tab" '{ printf "%-60s\t%-20s\t%s\t%s\t%s\n", substr($2, 0, 60), substr($4, 0, 20), $1, $3, $5 }' |
-    fzf >"$tmp"
+    awk -F"$tab" '{ printf "%-60s\t%-15s\t%s\t%s\t%s\n", substr($2, 0, 60), substr($4, 0, 15), $1, $3, $5 }' |
+    fzf -d '\t' --with-nth=1,2 >"$tmp"
 
 if [ -s "$tmp" ]; then
     cut -f4,5 "$tmp" | sed 's/\t$//' | rev | cut -f1 | rev | xargs setsid -f open
