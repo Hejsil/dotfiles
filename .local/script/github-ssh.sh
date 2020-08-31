b19d35cc2b60f66572caf1010ca2e141323c502d
Jimmi Holst Christensen <jhc@dismail.de>
2020-08-31 09:11:05 +0200

    Remove unused scripts

--- .local/script/github-ssh.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-git config --global user.email 'jhc@dismail.de'
-git config --global user.name 'Jimmi Holst Christensen'
-ssh-keygen -t rsa -b 4096 -C 'jhc@dismail.de'
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
-xclip -sel clip < ~/.ssh/id_rsa.pub
--- .local/script/my-ip.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-curl ifconfig.co
