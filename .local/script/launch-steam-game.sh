57f051e187036431aa2ac1c0cf0dc003116febfe
Jimmi Holst Christensen <jhc@dismail.de>
2020-09-12 20:11:37 +0200

    More actions for steam launcher

--- .local/script/launch-steam-game.sh
+++ .local/script/launch-steam-game.sh
@@ -3,4 +3,6 @@
 id=$(steam-list.sh | fzf -d '\t' --with-nth=1 | tail -n 1 | cut -f2)
 [ -z "$id" ] && exit 0
 
-setsid -f steam "steam://run/$id"
+action=$(printf 'run\nuninstall\n' | fzf | tail -n 1)
+[ -z "$action" ] && exit 0
+setsid -f steam "steam://$action/$id"
