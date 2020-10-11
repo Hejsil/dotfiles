f06512f301b9ba203a46fd18181d3cd4e53bbfa8
Jimmi Holst Christensen <jhc@dismail.de>
2020-10-12 01:19:26 +0200

    Fix more rss stuff

--- .local/script/rss-open-all-in-browser.sh
+++ .local/script/rss-open-all-in-browser.sh
@@ -18,7 +18,7 @@ done
 tmp=$(mktemp)
 tab=$(printf '\t')
 rss-list.sh -u |
-while IFS=$tab read -r file _ link _ _ _ enclosure; do
+while IFS=$tab read -r file _ _ link _ _ _ enclosure; do
     printf '%s\t%s\t%s\t%s\n' "$(opener.sh "$link")" "$file" "$link" "$enclosure"
 done | grep "^$BROWSER" | head -n 50 >"$tmp"
 
