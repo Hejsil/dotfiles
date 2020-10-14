85b58198b89db83ea30812511aa74f97fcd097bb
Jimmi Holst Christensen <jhc@dismail.de>
2020-10-14 19:02:50 +0200

    Remove useless scripts and lines

--- .local/script/tra.sh
+++ .local/script/tra.sh
@@ -1,19 +1,3 @@
 #!/bin/sh
-
-program=${0##*/}
-usage() {
-    echo "Usage: $program"
-}
-
-while [ -n "$1" ]; do
-    case $1 in
-        --) shift; break ;;
-        -h|--help) usage; exit 0 ;;
-        -*) usage; exit 1 ;;
-        *) break ;;
-    esac
-    shift
-done
-
 transmission-remote -a "$1" || exit 1
 notify-send 'Torrent added' "$1"
