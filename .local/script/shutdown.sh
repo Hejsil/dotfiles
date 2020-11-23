018e8f33ca9359f071485ff083be37bff2f717dc
Jimmi Holst Christensen <jhc@dismail.de>
2020-11-23 16:49:16 +0100

    No more hibernate

--- .local/script/shutdown.sh
+++ .local/script/shutdown.sh
@@ -1,11 +1,3 @@
 #!/bin/sh -e
 update.sh
-
-running_kernel=$(uname -r)
-disk_kernel=$(file -b /boot/vmlinuz-linux | sed 's/.*version //; s/ .*//')
-if [ "$running_kernel" = "$disk_kernel" ]; then
-    systemctl suspend
-else
-    systemctl poweroff
-fi
-
+systemctl poweroff
