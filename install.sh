cb159dd16a720c56da97c60740e577d8d51ed437
Jimmi Holst Christensen <jhc@dismail.de>
2020-11-23 16:43:41 +0100

    Add zls config

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+
 ln -snf "$(pwd)/.bash_profile" "$HOME/.bash_profile"
 ln -snf "$(pwd)/.bashrc" "$HOME/.bashrc"
 ln -snf "$(pwd)/.config" "$HOME/.config"
 ln -snf "$(pwd)/.ignore" "$HOME/.ignore"
 ln -snf "$(pwd)/.local/script" "$HOME/.local/script"
 ln -snf "$(pwd)/.local/share/anilist" "$HOME/.local/share/anilist"
+ln -snf "$(pwd)/.local/share/zls" "$HOME/.local/share/zls"
 ln -snf "$(pwd)/.profile" "$HOME/.profile"
 ln -snf "$(pwd)/.xsession" "$HOME/.xsession"
