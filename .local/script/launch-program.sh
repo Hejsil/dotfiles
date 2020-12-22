acd1e5abb5033d7a43b261a9e981bb3138b37164
Jimmi Holst Christensen <jhc@dismail.de>
2020-12-23 00:19:26 +0100

    Sleep before running program

--- .local/script/launch-program.sh
+++ .local/script/launch-program.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 setsid -f $(path-exes.sh | fzf --print-query | grep -v '^$' | tail -n 1)
+sleep 0.05s
--- .local/script/path-exes.sh
+++ .local/script/path-exes.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-echo "$PATH" | tr ':' '\n' | xargs -I% find -L % -type f |
+echo "$PATH" | tr ':' '\n' | xargs -I% find -L % -type f 2>/dev/null |
     xargs basename -a | sort -u
 
