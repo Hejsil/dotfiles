afacf47d0c005aec740d7e76f005730e97045956
Jimmi Holst Christensen <jhc@dismail.de>
2021-01-02 16:51:33 +0100

    Now local music bot

--- .local/script/bot.sh
+++ .local/script/bot.sh
@@ -1,23 +1,78 @@
-#!/bin/sh
-folder="$HOME/downloads/discord-bot"
-mkdir -p "$folder"
+#!/bin/sh -x
+folder="$HOME/.cache/bot"
+music_folder="$folder/music"
+video_folder="$folder/video"
+queue="$folder/queue"
+playing="$folder/playing"
+mpv_socket="/tmp/mpvsocket"
 
-mash_tools listener 'dispatcher' |
-    jq --unbuffered -r 'select((.op==0 and .t=="MESSAGE_CREATE")) | .d.content' |
-    grep --line-buffered '^::' |
-    sed -Eu 's/^:://' |
-while read -r command link; do
-    case $command in
+mkdir -p "$music_folder"
+mkdir -p "$video_folder"
+
+printf '' > "$queue"
+function queue_pop() {
+    res=$(head -n1 "$queue")
+    sed -i '1d' "$queue"
+    printf '%s\n' "$res"
+}
+
+function download() {
+    cmd=$1
+    link=$2
+    IFS=$(printf '\a')
+    format="$(echo "$cmd" | sed -E -e 's/pt?/bestaudio/' -e 's/vt?/best/')"
+    download_folder="$(echo "$cmd" | sed -E -e 's/pt?/music/' -e 's/vt?/video/')"
+    template="$folder/$download_folder/%(title)s %(id)s ($format)"
+    youtube-dl -i -f "$format" -o "${template}${IFS}%(id)s" --get-filename "$link" |
+    while read -r file_name id; do
+        if ! [ -e "$file_name" ]; then
+            youtube-dl -i -f "$format" -o "$template" "$id" >&2
+        fi
+        printf '%s\n' "$file_name"
+    done
+
+}
+
+IFS=$(printf '\t')
+rg --line-buffered -o '!(\w+)\s*([^ ]*)' -r "\$1$(printf '\t')\$2" |
+while read -r cmd link; do
+    printf "!%s %s\n" "$cmd" "$link"
+    case $cmd in
         p|v)
-            format="$(echo "$command" | sed -e 's/p/bestaudio/' -e 's/v/best/')"
-            template="$folder/%(title)s %(id)s ($format)"
-            file_name="$(youtube-dl --no-playlist -o "$template" --get-filename "$link")"
-            if ! [ -e "$file_name" ]; then
-                youtube-dl -f "$format" --no-playlist  -o "$template" "$link"
-            fi
-
-            echo "$file_name"
+            download "$cmd" "$link" | cat "$queue" - | sponge "$queue"
             ;;
-        s) killall mpv ;;
+        pt|vt)
+            download "$cmd" "$link" | cat - "$queue" | sponge "$queue"
+            ;;
+        s)
+            printf '{ "command": ["stop"] }\n' |
+                socat -u - "$mpv_socket"
+            ;;
     esac
-done | xargs -n 1 -d'\n' mpv --fullscreen
+done &
+
+mpv "--input-ipc-server=$mpv_socket" --force-window --idle --loop-playlist=no &
+
+while ! printf '' | socat -u "$mpv_socket" -; do :; done |
+    rg --line-buffered -F '{"event":"idle"}' |
+while read -r _; do
+    file_name=$(queue_pop)
+
+    # If no file has been queue, then we pick a file that we have already
+    # downloaded at random.
+    while [ -z "$file_name" ]; do
+        file_name=$(find "$music_folder" -type f | sort -R | head -n1)
+
+        # We might not have downloaded any files yet. Wait for changes
+        # to the folder and try again.
+        [ -z "$file_name" ] && inotifywait "$music_folder"
+    done
+
+    printf '%s\n' "$file_name" > "$playing"
+    printf '{ "command": ["loadfile", "%s"] }\n' "$file_name" |
+        socat -u - "$mpv_socket"
+done
+
+printf "Yikes!\n"
+wait
+
