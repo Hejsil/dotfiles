22448e08377a97235745abb5b8da561c40defc0f
Jimmi Holst Christensen <jhc@dismail.de>
2021-01-27 17:30:13 +0100

    Better handle rss reading

--- .local/script/rss-open-all-in-browser.sh
+++ .local/script/rss-open-all-in-browser.sh
@@ -4,6 +4,6 @@ tab=$(printf '\t')
 rss-list.sh -u |
     while IFS=$tab read -r file _ _ link _ _ _ enclosure; do
         printf '%s\t%s\t%s\t%s\n' "$(opener.sh "$link")" "$file" "$link" "$enclosure"
-    done | grep "^$BROWSER" | head -n 50 | choose -f '\t' -o '\t' 1 -1 | tr '\t' '\n' |
+    done | grep "^$BROWSER" | head -n 50 | cut -f2,3,4 | tr '\t' '\n' |
     xargs -d'\n' xargs tsp rss-open-read.sh
 
--- .local/script/rss-open-read.sh
+++ .local/script/rss-open-read.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-input=$(while [ -n "$1" ] && [ -n "$2" ]; do
-    printf '%s\t%s\n' "$1" "$2"
-    shift; shift
-done)
-printf '%s' "$input" | cut -f2 | xargs -d '\n' open || exit 1
+input=$(for arg in "$@"; do
+    printf '%s\n' "$arg"
+done | paste - - -)
+
+printf '%s' "$input" | cut -f3 | xargs -d '\n' open ||
+    printf '%s' "$input" | cut -f2 | xargs -d '\n' open || exit 1
 printf '%s' "$input" | cut -f1 | xargs -d '\n' -I{} mv {} "$HOME/.cache/rss/read"
 
