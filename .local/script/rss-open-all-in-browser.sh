db70833a3a74680892e0879ba085f4abdbf04ead
Jimmi Holst Christensen <jhc@dismail.de>
2021-02-25 18:50:59 +0100

    Use sfeed_update instead of rss.sh

--- .local/script/rss-open-all-in-browser.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-tab=$(printf '\t')
-rss-list.sh -u |
-    while IFS=$tab read -r file _ _ link _ _ _ enclosure; do
-        printf '%s\t%s\t%s\t%s\n' "$(opener.sh "$link")" "$file" "$link" "$enclosure"
-    done | grep "^$BROWSER" | head -n 50 | cut -f2,3,4 | tr '\t' '\n' |
-    xargs -d'\n' xargs tsp rss-open-read.sh
-
