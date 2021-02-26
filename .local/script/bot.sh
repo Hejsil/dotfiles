ee49537fc2174f69b364ed9a37b6bf707962e6e1
Jimmi Holst Christensen <jhc@dismail.de>
2021-02-26 18:57:03 +0100

    Have bot only accept creative commons music

--- .local/script/bot.sh
+++ .local/script/bot.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -x
+#!/bin/sh
 folder="$HOME/.cache/bot"
 music_folder="$folder/music"
 video_folder="$folder/video"
@@ -17,43 +17,56 @@ queue_pop() {
 }
 
 download() {
-    cmd=$1
-    link=$2
+    user=$1
+    cmd=$2
+    link=$3
+
+    case "$cmd" in
+        p | pt)
+            format="bestaudio"
+            download_folder="music"
+            ;;
+        v | vt)
+            format="best"
+            download_folder="video"
+            ;;
+        *) return ;;
+    esac
+
     IFS=$(printf '\a')
-    format="$(echo "$cmd" | sed -E -e 's/pt?/bestaudio/' -e 's/vt?/best/')"
-    download_folder="$(echo "$cmd" | sed -E -e 's/pt?/music/' -e 's/vt?/video/')"
     template="$folder/$download_folder/%(title)s %(id)s ($format)"
-    youtube-dl -i -f "$format" -o "${template}${IFS}%(id)s" --get-filename "$link" |
-        while read -r file_name id; do
+    youtube-dl -i -f "$format" -o "${template}${IFS}%(id)s${IFS}%(license)s${IFS}%(title)s" --get-filename "$link" |
+        while read -r file_name id license title; do
+            if [ "${license#*Creative Commons}" == "$license" ]; then
+                notify-send "Invalid license!" "$(printf 'user: %s\ntitle: %s\nlicense: %s' \
+                    "$user" "$title" "$license")"
+                continue
+            fi
+
             if ! [ -e "$file_name" ]; then
                 youtube-dl -i -f "$format" -o "$template" "$id" >&2
             fi
             printf '%s\n' "$file_name"
+            notify-send "Song queued" "$(printf 'user: %s\ntitle: %s\n' \
+                "$user" "$title")"
         done
 
 }
 
 IFS=$(printf '\t')
-rg --line-buffered -o '!(\w+)\s*([^ ]*)' -r "\$1$(printf '\t')\$2" |
-    while read -r cmd link; do
+rg --line-buffered -o '([^ ]+)\s+!(\w+)\s+([^ ]*)' -r "\$1${IFS}\$2${IFS}\$3" |
+    while read -r user cmd link; do
         printf "!%s %s\n" "$cmd" "$link"
         case $cmd in
-        p | v)
-            download "$cmd" "$link" | cat "$queue" - | sponge "$queue"
-            ;;
-        pt | vt)
-            download "$cmd" "$link" | cat - "$queue" | sponge "$queue"
-            ;;
-        s)
-            printf '{ "command": ["stop"] }\n' |
-                socat -u - "$mpv_socket"
-            ;;
+            p | v) download "$user" "$cmd" "$link" | cat "$queue" - | sponge "$queue" ;;
+            pt | vt) download "$user" "$cmd" "$link" | cat - "$queue" | sponge "$queue" ;;
+            s) printf '{ "command": ["stop"] }\n' | socat -u - "$mpv_socket" ;;
         esac
     done &
 
 mpv "--input-ipc-server=$mpv_socket" --force-window --idle --loop-playlist=no &
 
-while ! printf '' | socat -u "$mpv_socket" -; do :; done |
+while ! printf '' | socat -u "$mpv_socket" - 2>/dev/null; do :; done |
     rg --line-buffered -F '{"event":"idle"}' |
     while read -r _; do
         file_name=$(queue_pop)
@@ -71,6 +84,8 @@ while ! printf '' | socat -u "$mpv_socket" -; do :; done |
         printf '%s\n' "$file_name" >"$playing"
         printf '{ "command": ["loadfile", "%s"] }\n' "$file_name" |
             socat -u - "$mpv_socket"
+        title=$(basename "$file_name" | rg '^(.*) \w+ \(\w+\)$' -r '$1')
+        notify-send "Now playing" "$title"
     done
 
 printf "Yikes!\n"
