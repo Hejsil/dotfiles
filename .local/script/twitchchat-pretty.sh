2c8d8dd85e8f8efcae950f7156733d94fb8a8749
Jimmi Holst Christensen <jhc@dismail.de>
2021-02-26 18:58:38 +0100

    Reformat pretty-twitchchat

--- .local/script/twitchchat-pretty.sh
+++ .local/script/twitchchat-pretty.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+clear
 tput civis
 trap 'tput cnorm' EXIT
 
@@ -7,22 +8,21 @@ TAB=$(printf '\t')
 rg --line-buffered \
     '#\w+: \d{2}/\d{2}/\d{2} (\d{2}:\d{2}) <(\w+)> (.*)$' \
     -r "\$1$TAB\$2$TAB\$3" |
-while read -r clock name msg; do
-    if [ "$last_name" = "$name" ]; then
-        if [ "$last_clock" = "$clock" ]; then
-            printf '\n   >> '
+    while read -r clock name msg; do
+        if [ "$last_name" = "$name" ]; then
+            if [ "$last_clock" = "$clock" ]; then
+                printf '\n   >> '
+            else
+                printf '\n%s ' "$clock"
+            fi
         else
-            printf '\n%s ' "$clock"
+            name_hash=$(printf "%d" "0x$(printf '%s' "$name" | md5sum | nawk '{printf "%.8s", $1}')")
+            name_color=$(printf '%s' "$name" | lolcat --force "--seed=$name_hash")
+            printf '\n%s <%s>\n      ' "$clock" "$name_color"
         fi
-    else
-        name_hash=$(printf "%d" "0x$(printf '%s' "$name" | md5sum | nawk '{printf "%.8s", $1}')")
-        name_color=$(printf '%s' "$name" | lolcat --force "--seed=$name_hash")
-        printf '\n%s <%s>\n      ' "$clock" "$name_color"
-    fi
-    printf '%s' "$msg" | fmt -w "$(($(tput cols) - 6))"  |
-        sed '2,1000s/^/      /' |
-        head -c -1
-    last_name=$name
-    last_clock=$clock
-done
-
+        printf '%s' "$msg" | fmt -w "$(($(tput cols) - 6))" |
+            sed '2,1000s/^/      /' |
+            head -c -1
+        last_name=$name
+        last_clock=$clock
+    done
