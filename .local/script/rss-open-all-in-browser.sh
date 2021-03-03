f92de8afbae611b278fb852bd080036bb2f5ea56
Jimmi Holst Christensen <jhc@dismail.de>
2021-03-03 22:01:26 +0100

    Better browser rss script

--- .local/script/rss-open-all-in-browser.sh
+++ .local/script/rss-open-all-in-browser.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 tab=$(printf '\t')
-rss-list.sh -u |
-    while IFS=$tab read -r file _ _ link _ _ _ enclosure; do
+rss-list.sh -u | cut -f1,4,8 |
+    while IFS=$tab read -r file link enclosure; do
         printf '%s\t%s\t%s\t%s\n' "$(opener.sh "$link")" "$file" "$link" "$enclosure"
     done | grep "^$BROWSER" | head -n 50 | cut -f2,3,4 | tr '\t' '\n' |
     xargs -d'\n' xargs tsp rss-open-read.sh
-
