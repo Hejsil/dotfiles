d6a3a01f3be6a19dd72498b42d2fc8bd5a67ad4e
Komari Spaghetti <3759175+Hejsil@users.noreply.github.com>
2021-04-26 22:31:20 +0200

    Better gtw theme

--- .config/template/oomox-xresources-reverse/assets/unsed.sh
+++ .config/template/oomox-xresources-reverse/assets/unsed.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sed -i \
-         -e 's/rgb(0%,0%,0%)/#{{base00-hex}}/g' \
+         -e 's/rgb(0%,0%,0%)/#{{base01-hex}}/g' \
          -e 's/rgb(100%,100%,100%)/#{{base05-hex}}/g' \
     -e 's/rgb(50%,0%,0%)/#{{base00-hex}}/g' \
      -e 's/rgb(0%,50%,0%)/#{{base0A-hex}}/g' \
