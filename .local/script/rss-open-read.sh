b9517a7d6fb030929293ea37b9fd0c7e65519385
Komari Spaghetti <3759175+Hejsil@users.noreply.github.com>
2021-05-30 22:47:50 +0200

    Update rss folder

--- .local/script/rss-open-read.sh
+++ .local/script/rss-open-read.sh
@@ -7,5 +7,5 @@ done | paste - - - >"$input"
 
 cut -f2 "$input" | xargs -d '\n' open ||
     cut -f3 "$input" | xargs -d '\n' open || exit 1
-cut -f1 "$input" | xargs -d '\n' -I{} mv {} "$HOME/.cache/rss/read"
+cut -f1 "$input" | xargs -d '\n' -I{} mv {} "$HOME/.local/share/rss/read"
 rm "$input"
