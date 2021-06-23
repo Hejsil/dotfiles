5baad897f0e481e2ce303c7dd04554490d9e90a7
Komari Spaghetti <3759175+Hejsil@users.noreply.github.com>
2021-06-23 20:05:25 +0200

    Small cleanups

--- .local/script/wall.sh
+++ .local/script/wall.sh
@@ -2,4 +2,6 @@
 
 folder=$1
 monitors=$(xrandr | grep -c ' connected')
-find "$folder" -type f | sort -R | head -n "$monitors" | sed 's/^/-z\n/' | xargs -d '\n' setroot
+find "$folder" -type f | sort -R |
+    head -n "$monitors" | sed 's/^/-z\n/' |
+    xargs -d '\n' setroot
