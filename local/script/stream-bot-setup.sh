d03b53c0c437592fcc797404dc5746c821fbb391
Komari Spaghetti <3759175+Hejsil@users.noreply.github.com>
2021-07-12 21:18:43 +0200

    Use default MEGA folder

--- local/script/stream-bot-setup.sh
+++ local/script/stream-bot-setup.sh
@@ -12,7 +12,7 @@ st -e twitchbot-open-links.sh komarispaghetti 'tor-browser --allow-remote' \
 # st -e twitchbot-tm35-instance.sh komarispaghetti /tmp/twitch \
 #     "$HOME/repo/metronome/zig-cache/bin" \
 #     "$HOME/repo/melonDS/build/melonDS" \
-#     "$HOME/mega/documents/roms/pokemon/gen5/pokemon_black2_ireo.nds" \
+#     "$HOME/MEGA/documents/roms/pokemon/gen5/pokemon_black2_ireo.nds" \
 #     "$HOME/repo/metronome/black2-twitch-mods.nds" &
 
 st -e tail -f '/tmp/twitch/komarispaghetti' &
