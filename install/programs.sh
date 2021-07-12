5faf0414e5d948f84499ced824471f87ce5dff4d
Komari Spaghetti <3759175+Hejsil@users.noreply.github.com>
2021-07-12 21:18:16 +0200

    Script for reinstalling all programs

---
+++ install/programs.sh
@@ -0,0 +1,54 @@
+#!/bin/sh -e
+
+exe="$(realpath "$0")"
+here="$(dirname "$exe")/.."
+
+git_clone() {
+    git clone --depth 1 --shallow-submodules --recursive "$@"
+}
+
+install_paru() {
+    folder="$(mktemp -d)"
+
+    sudo pacman -S --needed base-devel
+    [ -e "$folder" ] && rm -rf "$folder"
+    git_clone 'https://aur.archlinux.org/paru.git' "$folder"
+
+    cd "$folder"
+    makepkg -si --noconfirm
+    paru -S --noconfirm paru
+}
+
+install_zig() {
+    repo="$1"
+    folder="$(mktemp -d)"
+
+    git_clone "https://github.com/Hejsil/$repo.git" "$folder"
+    zig build --build-file "$folder/build.zig" -Drelease &&
+        sudo zig build --build-file "$folder/build.zig" -Drelease --prefix /usr/local
+}
+
+install_st() {
+    folder="$HOME/repo/st-flexipatch"
+    rm -rf "$folder"
+    mkdir -p "$folder"
+    git_clone 'https://github.com/bakkeby/st-flexipatch.git' "$folder"
+
+    cd "$folder"
+    rm config.def.h patches.def.h config.mk Makefile
+    ln "$here/config/st-flexipatch/config.def.h" 'config.def.h'
+    ln "$here/config/st-flexipatch/patches.def.h" 'patches.def.h'
+    ln "$here/config/st-flexipatch/config.mk" 'config.mk'
+    ln "$here/config/st-flexipatch/Makefile" 'Makefile'
+
+    make
+    sudo make install
+}
+
+install_paru
+paru -S --needed --noconfirm - <"$here/config/installed-programs"
+
+install_zig 'lemonbar-maker'
+install_zig 'anilist'
+
+install_st
