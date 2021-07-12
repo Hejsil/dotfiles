45a8e5842545e3f268ac0d5d97e0c03c6dd38057
Komari Spaghetti <3759175+Hejsil@users.noreply.github.com>
2021-07-12 18:37:14 +0200

    Unhide config and local folder

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 . "$(pwd)/.profile"
+
+ln -snf "$(pwd)/config" "$HOME/.config"
+ln -snf "$(pwd)/local" "$HOME/.local"
+
 ln -snf "$(pwd)/.bash_profile" "$HOME/.bash_profile"
 ln -snf "$(pwd)/.bashrc" "$HOME/.bashrc"
-ln -snf "$(pwd)/.config" "$HOME/.config"
 ln -snf "$(pwd)/.ignore" "$HOME/.ignore"
-ln -snf "$(pwd)/.local" "$HOME/.local"
 ln -snf "$(pwd)/.profile" "$HOME/.profile"
 ln -snf "$(pwd)/.xsession" "$HOME/.xsession"
 ln -sf "$(which open)" "$HOME/.local/script/xdg-open"
