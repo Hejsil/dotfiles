cc79b840934f5cecbd8914760acc1c0ee2a9e437
Komari Spaghetti <3759175+Hejsil@users.noreply.github.com>
2021-07-12 18:44:02 +0200

    Unhide the other hidden files in repo root

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-. "$(pwd)/.profile"
+. "$(pwd)/config/profile"
 
 ln -snf "$(pwd)/config" "$HOME/.config"
 ln -snf "$(pwd)/local" "$HOME/.local"
 
-ln -snf "$(pwd)/.bash_profile" "$HOME/.bash_profile"
-ln -snf "$(pwd)/.bashrc" "$HOME/.bashrc"
-ln -snf "$(pwd)/.ignore" "$HOME/.ignore"
-ln -snf "$(pwd)/.profile" "$HOME/.profile"
-ln -snf "$(pwd)/.xsession" "$HOME/.xsession"
-ln -sf "$(which open)" "$HOME/.local/script/xdg-open"
+ln -snf "$(pwd)/config/profile" "$HOME/.bash_profile"
+ln -snf "$(pwd)/config/profile" "$HOME/.profile"
+
+ln -snf "$(pwd)/config/bashrc" "$HOME/.bashrc"
+ln -snf "$(pwd)/config/xorg/init" "$HOME/.xsession"
+
 generate xresources
 generate dunstrc
 generate gtk_theme
