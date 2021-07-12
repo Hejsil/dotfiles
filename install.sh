50b4dd9d97e9a604d19bf2c5688c1a661dd72b02
Komari Spaghetti <3759175+Hejsil@users.noreply.github.com>
2021-07-12 21:19:34 +0200

    Add pacman config

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "$0")" || exit 1
 
-sudo install/systemd-services.sh
-sudo install/paru.sh
+install/programs.sh
+install/system-files.sh
 install/linkup.sh
 install/generate-themes.sh
