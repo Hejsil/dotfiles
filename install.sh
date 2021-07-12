f30caf2f18f35162cc9fe984018a76945827ff5b
Komari Spaghetti <3759175+Hejsil@users.noreply.github.com>
2021-07-12 19:58:41 +0200

    Provide more scripts for installing stuff

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -e
 
-sudo install/install-system.sh
+cd "$(dirname "$0")" || exit 1
+
+sudo install/systemd-services.sh
+sudo install/paru.sh
 install/linkup.sh
 install/generate-themes.sh
