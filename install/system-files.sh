19cf49397b7ffe88220a79b99af02912b263eb01
Komari Spaghetti <3759175+Hejsil@users.noreply.github.com>
2021-07-15 16:41:43 +0000

    Add grub config

--- install/system-files.sh
+++ install/system-files.sh
@@ -4,3 +4,4 @@ cd "$(dirname "$0")/.."
 
 sudo cp -ri 'config/systemd-system/getty@tty1.service.d' '/etc/systemd/system/'
 sudo cp -ri 'config/pacman.conf' '/etc/pacman.conf'
+sudo cp -ri 'config/grub' '/etc/default/grub'
