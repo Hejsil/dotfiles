671c09fc3ac29f80c0673cafee1742e86082a7b1
Komari Spaghetti <3759175+Hejsil@users.noreply.github.com>
2021-09-09 17:15:59 +0200

    screenshot: Use slop and import instead of maim

--- local/script/screenshot.sh
+++ local/script/screenshot.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 file="$1/$(date '+%y-%m-%dT%H:%M:%S').png"
-maim -s "$file" || exit 1
+
+selection=$(slop -f "%g") || exit 1
+import -window root -crop "$selection" "$file"
+
+optipng "$file"
 xclip -selection clipboard -t image/png -i "$file"
 notify-send 'Screenshot Taken' "$file"
-optipng "$file"
