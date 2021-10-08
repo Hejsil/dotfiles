8ae6a4f6458a0e9f28e6802f3edf79e597227784
Komari Spaghetti <3759175+Hejsil@users.noreply.github.com>
2021-10-08 16:39:53 +0200

    Use stack command for wall.sh

--- local/script/wall.sh
+++ local/script/wall.sh
@@ -1,7 +1,13 @@
 #!/bin/sh -e
 
 folder=$1
+stack_folder='/tmp/random-wallpapers'
 monitors=$(xrandr | grep -c ' connected')
-find "$folder" -type f | sort -R |
-    head -n "$monitors" | sed 's/^/-z\n/' |
-    xargs -d '\n' setroot
+seq "$monitors" | while read -r; do
+    stack pop "$stack_folder" || {
+        find "$folder" -type f | sort -R | while read -r file; do
+            echo "$file" | stack push "$stack_folder"
+        done
+        stack pop "$stack_folder"
+    }
+done | sed 's/^/-z\n/' | xargs -d '\n' setroot
