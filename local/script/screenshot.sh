f29e982c0cb3415dbf8e89a49c6594af0e85998d
Jimmi Holst Christensen <jhc@dismail.de>
2021-12-07 19:19:22 +0100

    Don't optimize screenshots

--- local/script/screenshot.sh
+++ local/script/screenshot.sh
@@ -4,6 +4,5 @@ file="$1/$(date '+%y-%m-%dT%H:%M:%S').png"
 selection=$(slop -f "%g") || exit 1
 import -window root -crop "$selection" "$file"
 
-optipng "$file"
 xclip -selection clipboard -t image/png -i "$file"
 notify-send 'Screenshot Taken' "$file"
