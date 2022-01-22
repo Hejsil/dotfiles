9dbc32ee4235f6bc2f6e71a72b3f4a6df7235684
Jimmi Holst Christensen <jhc@dismail.de>
2022-01-22 01:05:02 +0100

    Small changes

--- install/system-files.sh
+++ install/system-files.sh
@@ -5,5 +5,5 @@ cd "$(dirname "$0")/.."
 sudo cp -ri 'config/systemd-system/getty@tty1.service.d' '/etc/systemd/system/'
 sudo cp -ri 'config/pacman.conf' '/etc/pacman.conf'
 sudo cp -ri 'config/grub' '/etc/default/grub'
-sudo cp -ri 'config/sysctl.d/' '/etc/sysctl.d/'
-sudo cp -ri 'config/xorg.conf.d/' '/etc/X11/xorg.conf.d/'
+sudo cp -ri config/sysctl.d/* '/etc/sysctl.d/'
+sudo cp -ri config/xorg.conf.d/* '/etc/X11/xorg.conf.d/'
