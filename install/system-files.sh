43966647ce991fa10b71c4ec3a3d8a8fc97c7a8f
Jimmi Holst Christensen <jhc@dismail.de>
2022-08-07 13:37:01 +0200

    No longer install system journald.conf

--- install/system-files.sh
+++ install/system-files.sh
@@ -4,7 +4,6 @@ cd "$(dirname "$0")/.."
 
 sudo cp -ri 'config/grub' '/etc/default/grub'
 sudo cp -ri 'config/pacman.conf' '/etc/pacman.conf'
-sudo cp -ri 'config/systemd/journald.conf' '/etc/systemd/journald.conf'
 sudo cp -ri 'config/systemd-system/getty@tty1.service.d' '/etc/systemd/system/'
 sudo cp -ri config/modprobe.d/* '/etc/modprobe.d/'
 sudo cp -ri config/sysctl.d/* '/etc/sysctl.d/'
