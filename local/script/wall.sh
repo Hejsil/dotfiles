4e2658a95a50b50f86f26b4719495bbfeb8cff02
Jimmi Holst Christensen <jhc@dismail.de>
2022-09-07 09:54:59 +0200

    No longer cycle wallpapers on a timer

--- local/script/wall.sh
+++ local/script/wall.sh
@@ -1,29 +1,18 @@
 #!/bin/sh -e
 
 folder=$1
-stack_folder='/tmp/wallpapers/stack'
-scaled_folder='/tmp/wallpapers/images'
-
-mkdir -p "$scaled_folder"
-
-pop() {
-    stack pop "$stack_folder" || {
-        find "$folder" -type f | sort -R | while read -r file; do
-            echo "$file" | stack push "$stack_folder"
-        done
-        stack pop "$stack_folder"
-    }
-}
+screens=$(mktemp)
+images=$(mktemp)
 
 xrandr --listactivemonitors |
-    rg -o '^ \d+: [^ ]+ (\d+)/\d+x(\d+).*  (.+)$' -r '$3 $1 $2' |
-    while read -r id w h; do
-        file=$(pop)
-        scaled="$scaled_folder/$(basename "$file")-$id"
-        cache -f "$file" -o "$scaled" -- \
-            convert "$file" -resize "${w}x${h}^" -quality 100 "jpg:$scaled"
-        echo '--output'
-        echo "$id"
-        echo '--center'
-        echo "$scaled"
-    done | xargs -d '\n' xwallpaper
+    rg -o '^ \d+: [^ ]+ \d+/\d+x\d+.*  (.+)$' -r '$1' >"$screens"
+count=$(wc -l <"$screens")
+
+find "$folder" -type f | sort -R | head -n "$count" >"$images"
+
+paste "$screens" "$images" |
+    nawk '{printf "--output\n%s\n--zoom\n%s\n", $1, $2 }' |
+    xargs -d '\n' xwallpaper
+
+rm "$screens"
+rm "$images"
