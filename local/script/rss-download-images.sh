9e10b630501d4d8df78e187326fea8e39ee93fec
Jimmi Holst Christensen <jhc@dismail.de>
2022-10-21 16:07:03 +0200

    Use gallery-dl for rss-download-images

--- local/script/rss-download-images.sh
+++ local/script/rss-download-images.sh
@@ -4,38 +4,14 @@ rss.sh
 
 download_dir="$HOME/downloads/images"
 mkdir -p "$download_dir"
-tmp_template="$download_dir/XXXXXX"
-
+cd "$download_dir"
 tab=$(printf '\t')
-a=$(printf '\a')
-rss-list.sh -u | tr "$tab" "$a" | while IFS=$a read -r file _ _ link desc _; do
-    case $link in
-        *konachan.net*)
-            curl -s "$link" |
-                grep -o 'href="[^"]*" id="highres"' |
-                cut -d'"' -f2
-            # Also output the link. This is done, so that if the site
-            # has no images, it will still be marked as read.
-            echo "$link"
-            ;;
-        *e-shuushuu.net*)
-            # shuushuu started using https only, but didn't update their rss feed...
-            echo "https:${link#http:}"
-            ;;
-        *www.reddit.com* | *pixiv.net*)
-            echo "$desc" | grep -oE '\w+:\/\/[-a-zA-Z0-9:@;?&=\/%\+\.\*!'"'"'\(\),\$_\{\}\^~`#|]+'
-            ;;
-    esac | sed "s$tab^$tab$file$a$tab"
-done | while IFS=$a read -r file link; do
-    tmp=$(mktemp "$tmp_template")
-    curl "$link" >"$tmp" || {
-        rm "$tmp"
-        continue
-    }
 
-    mv "$file" "$HOME/.local/share/rss/read/"
-    case $(file -b --mime-type "$tmp") in
-        image/*) ;;
-        *) rm "$tmp" ;;
-    esac
-done
+filter="www\.(pixiv\.net|artstation\.com)/artwork"
+image_rss_entries=$(rss-list.sh -u | cut "-d$tab" -f1,4 | rg "^[^\t]*\t.*$filter")
+if [ -z "$image_rss_entries" ]; then
+    exit
+fi
+
+echo "$image_rss_entries" | cut "-d$tab" -f2 | xargs -d'\n' gallery-dl || exit 1
+echo "$image_rss_entries" | cut "-d$tab" -f1 | xargs -d'\n' -I% mv % "$HOME/.local/share/rss/read/"
