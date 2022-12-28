e9edf6bcf064f3020b92e1dc44af948758313e6e
Jimmi Holst Christensen <jhc@dismail.de>
2022-12-28 19:58:05 +0100

    Filter nyaa links to open properly in browser

--- local/script/rss-open-all-in-browser.sh
+++ local/script/rss-open-all-in-browser.sh
@@ -4,5 +4,7 @@ tab=$(printf '\t')
 rss-list.sh -u | cut -f1,4,8 |
     while IFS=$tab read -r file link enclosure; do
         printf '%s\t%s\t%s\t%s\n' "$(printf '%s\t\n' "$link" | opener)" "$file" "$link" "$enclosure"
-    done | grep "^$BROWSER" | head -n 50 | cut -f2,3,4 | tr '\t' '\n' |
+    done | grep "^$BROWSER" | head -n 5 | cut -f2,3,4 | tr '\t' '\n' |
+    # Some links can be modified to open better in the browser
+    sd 'https://nyaa.si/download/(\w+).torrent' 'https://nyaa.si/view/$1' |
     exec xargs -d'\n' xargs tsp rss-open-read.sh
