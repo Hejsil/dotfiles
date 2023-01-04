fd084feea7319cfedec1231be3c2b482eb7e35ea
Jimmi Holst Christensen <jhc@dismail.de>
2023-01-04 18:47:06 +0100

    Change order of rss-open-read

--- local/script/rss-open-read.sh
+++ local/script/rss-open-read.sh
@@ -5,7 +5,7 @@ for arg in "$@"; do
     printf '%s\n' "$arg"
 done | paste - - - >"$input"
 
-cut -f2 "$input" | xargs -d '\n' open ||
-    cut -f3 "$input" | xargs -d '\n' open || exit 1
+cut -f3 "$input" | xargs -d '\n' open ||
+    cut -f2 "$input" | xargs -d '\n' open || exit 1
 cut -f1 "$input" | xargs -d '\n' -I{} mv {} "$HOME/.local/share/rss/read"
 exec rm "$input"
