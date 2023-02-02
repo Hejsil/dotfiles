e0c9a312e561534da698fddba304b14de8f36360
Jimmi Holst Christensen <jhc@dismail.de>
2023-02-02 16:13:20 +0100

    record.sh: Turn video into gif when done

--- local/script/record.sh
+++ local/script/record.sh
@@ -1,39 +1,15 @@
 #!/bin/sh
 
-program=${0##*/}
-usage() {
-    echo "Usage: $program"
-}
-
-fps=60
-while [ -n "$1" ]; do
-    case $1 in
-        --)
-            shift
-            break
-            ;;
-        -f | --fps)
-            shift
-            fps=$1
-            ;;
-        -h | --help)
-            usage
-            exit 0
-            ;;
-        -*)
-            usage
-            exit 1
-            ;;
-        *) break ;;
-    esac
-    shift
-done
-
 output=$1
 [ -z "$output" ] && output="$HOME/video/$(date '+%y-%m-%dT%H:%M:%S').mp4"
 
-echo "$output"
+mkdir -p "$(dirname "$output")"
 slop -f '%x %y %w %h' | {
     read -r x y w h
-    exec ffmpeg -f x11grab -s "${w}x${h}" -i ":0.0+${x},${y}" -r "$fps" -vcodec libx264 "$output"
+    ffmpeg -f x11grab -s "${w}x${h}" -i ":0.0+${x},${y}" -r 60 -vcodec libx264 "$output"
 }
+
+output_gif=$(echo "$output" | sed 's/\.[^.]*$/.gif/')
+gifgen -o "$output_gif" "$output"
+echo "$output"
+echo "$output_gif"
