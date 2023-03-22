70505167869d0189f050f5ce4191511ee54ad46e
Jimmi Holst Christensen <git@hejsil.anonaddy.me>
2023-03-22 13:01:08 +0100

    Use systemd-run instead of task-spooler

--- local/script/launch-steam-game.sh
+++ local/script/launch-steam-game.sh
@@ -5,4 +5,4 @@ ids=$(steam-list.sh | fzf -m -d '\t' --with-nth=1 | cut -f2)
 
 action=$(printf 'run\nuninstall\n' | fzf | tail -n 1)
 [ -z "$action" ] && exit 0
-echo "$ids" | xargs -d'\n' -I% tsp steam "steam://$action/%"
+echo "$ids" | xargs -d'\n' -I% systemd-run --user steam "steam://$action/%"
--- local/script/rss-open-all-in-browser.sh
+++ local/script/rss-open-all-in-browser.sh
@@ -8,4 +8,4 @@ rss-list unread | cut -f1,4,8 |
     while IFS=$tab read -r file link enclosure; do
         printf '%s\t%s\t%s\t%s\n' "$(printf '%s\t\n' "$link" | opener)" "$file" "$link" "$enclosure"
     done | grep "^$BROWSER" | cut -f2,3,4 | tr '\t' '\n' |
-    exec xargs -d'\n' xargs tsp rss-open-read.sh
+    exec xargs -d'\n' xargs systemd-run --user rss-open-read.sh
