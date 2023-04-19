502dd952c96701cd84186d41ba56f1bd7be4af62
Jimmi Holst Christensen <git@hejsil.anonaddy.me>
2023-04-19 16:44:49 +0200

    rss: Only download images of certain sizes

--- local/script/rss-download-images.sh
+++ local/script/rss-download-images.sh
@@ -7,11 +7,15 @@ mkdir -p "$download_dir"
 cd "$download_dir"
 tab=$(printf '\t')
 
-filter="www\.(pixiv\.net|artstation\.com)/artwork"
+names='konachan|pixiv|artstation|danbooru\.donmai|yande'
+top_levels='com|net|us|re'
+filter="(https?://)(www\.)?($names)\.($top_levels)/(artwork|post)"
 image_rss_entries=$(rss-list unread | cut "-d$tab" -f1,4 | rg "^[^\t]*\t.*$filter")
 if [ -z "$image_rss_entries" ]; then
     exit
 fi
 
-echo "$image_rss_entries" | cut "-d$tab" -f2 | xargs -d'\n' gallery-dl || exit 1
+echo "$image_rss_entries" | cut "-d$tab" -f2 | xargs -d'\n' gallery-dl \
+    --filter 'vars().get("width", vars().get("image_width", 2560)) >= 2560 and \
+    vars().get("height", vars().get("image_height", 1440)) >= 1440' || exit 1
 echo "$image_rss_entries" | cut "-d$tab" -f1 | xargs -d'\n' -I% mv % "$HOME/.local/share/rss/read/"
