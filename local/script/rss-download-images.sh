2ae5139c8d37e454856452c03139fcb3f503f22c
Jimmi Holst Christensen <git@hejsil.anonaddy.me>
2023-11-06 11:51:17 +0100

    rss: Update urls

--- local/script/rss-download-images.sh
+++ local/script/rss-download-images.sh
@@ -9,7 +9,7 @@ tab=$(printf '\t')
 
 rss-list unread | cut "-d$tab" -f1,3,8 |
     rg '^[^\t]*\t.*https://api.dr.dk/podcasts' |
-    rg -v '^[^\t]*\t\d+\. \w{3} \d{4} kl\. 12:00' |
+    rg -v '^[^\t]*\t\d+\. \w{3}\. \d{4} kl\. 12:00' |
     cut "-d$tab" -f1 | xargs -d'\n' -I% mv % "$HOME/.local/share/rss/read/"
 
 names='konachan|pixiv|artstation|danbooru\.donmai|yande'
