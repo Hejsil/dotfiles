64499224ab824332fe5e826f0f5797aa2fb87509
Jimmi Holst Christensen <git@hejsil.anonaddy.me>
2023-11-17 11:58:30 +0100

    Update .gitignore

--- local/script/launch-steam-game.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-ids=$(steam-list.sh | fzf -m -d '\t' --with-nth=1 | cut -f2)
-[ -z "$ids" ] && exit 0
-
-action=$(printf 'run\nuninstall\n' | fzf | tail -n 1)
-[ -z "$action" ] && exit 0
-echo "$ids" | xargs -d'\n' -I% systemd-run --user steam "steam://$action/%"
