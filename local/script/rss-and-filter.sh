c1894865b827d284a8813d0af178c34a265ec16e
Jimmi Holst Christensen <git@hejsil.anonaddy.me>
2024-01-28 20:30:05 +0100

    scripts: Remove unused

--- local/script/rss-and-filter.sh
+++ local/script/rss-and-filter.sh
@@ -2,9 +2,6 @@
 
 rss.sh
 
-# download_dir="$HOME/downloads/images"
-# mkdir -p "$download_dir"
-# cd "$download_dir"
 tab=$(printf '\t')
 
 rss-list unread | cut "-d$tab" -f1,3,8 |
@@ -36,16 +33,4 @@ rss-list unread |
     rg '^[^\t]*\t\d+\t[^\t]* pushed to project branch \d+' |
     cut "-d$tab" -f1 | xargs -d'\n' -I% mv % "$HOME/.local/share/rss/read/"
 
-# names='konachan|pixiv|artstation|danbooru\.donmai|yande|e-shuushuu'
-# top_levels='com|net|us|re'
-# filter="(https?://)(www\.)?($names)\.($top_levels)/(artwork|post|images)"
-# image_rss_entries=$(rss-list unread | cut "-d$tab" -f1,4 | rg "^[^\t]*\t.*$filter")
-# if [ -z "$image_rss_entries" ]; then
-#     exit
-# fi
-
-# echo "$image_rss_entries" | cut "-d$tab" -f2 | xargs -d'\n' gallery-dl &
-# --filter 'vars().get("width", vars().get("image_width", 2560)) >= 2560 and
-# vars().get("height", vars().get("image_height", 1440)) >= 1440'
-# echo "$image_rss_entries" | cut "-d$tab" -f1 | xargs -d'\n' -I% mv % "$HOME/.local/share/rss/read/" &
 wait
--- local/script/rss-read.sh
+++ local/script/rss-read.sh
@@ -3,6 +3,6 @@
 tab=$(printf '\t')
 rss-list unread |
     nawk -F"$tab" '{ printf "%-14s\t%s\t%s\t%s\t%s\n", substr($7, 0, 14), $3, $1, $4, $8 }' |
-    fzf -m -d '\t' --with-nth=1,2 --preview 'COLUMNS=$FZF_PREVIEW_COLUMNS rss-to-md.sh {3}' |
+    fzf -m -d '\t' --with-nth=1,2 --preview 'COLUMNS=$FZF_PREVIEW_COLUMNS rss-to-md {3}' |
     cut -f3,4,5 | tr '\t' '\n' |
     exec xargs -d'\n' xargs systemd-run --user rss-open-read.sh
