2b3a61a6c947ed96222cf8e086c1f79ab8f78167
Jimmi Holst Christensen <git@hejsil.anonaddy.me>
2024-01-28 20:15:55 +0100

    env: Less variables for programs

--- local/script/rss-open-all-in-browser.sh
+++ local/script/rss-open-all-in-browser.sh
@@ -4,8 +4,7 @@ tab=$(printf '\t')
 rss-list unread | cut -f1,4,8 |
     # Some links can be modified to open better
     sd 'https://nyaa.si/download/(\w+).torrent' 'https://nyaa.si/view/$1' |
-    sd 'https://www.dr.dk/lyd/p4/radioavisen/[a-zA-Z0-9-]*' '' |
     while IFS=$tab read -r file link enclosure; do
         printf '%s\t%s\t%s\t%s\n' "$(printf '%s\t\n' "$link" | opener)" "$file" "$link" "$enclosure"
-    done | grep "^$BROWSER" | cut -f2,3,4 | head -n30 | tr '\t' '\n' |
+    done | grep "^chromium" | cut -f2,3,4 | head -n30 | tr '\t' '\n' |
     exec xargs -d'\n' xargs systemd-run --user rss-open-read.sh
