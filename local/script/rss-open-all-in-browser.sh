8ccb5dbfec5044537c1c57c97b7be299ee863f82
Jimmi Holst Christensen <git@hejsil.anonaddy.me>
2024-04-09 09:51:50 +0200

    browser: Switch to firefox

--- local/script/rss-open-all-in-browser.sh
+++ local/script/rss-open-all-in-browser.sh
@@ -6,5 +6,5 @@ rss-list unread | cut -f1,4,8 |
     sd 'https://nyaa.si/download/(\w+).torrent' 'https://nyaa.si/view/$1' |
     while IFS=$tab read -r file link enclosure; do
         printf '%s\t%s\t%s\t%s\n' "$(printf '%s\t\n' "$link" | opener)" "$file" "$link" "$enclosure"
-    done | grep "^chromium" | cut -f2,3,4 | sort -R | head -n10 | tr '\t' '\n' |
+    done | grep "^firefox" | cut -f2,3,4 | sort -R | head -n10 | tr '\t' '\n' |
     exec xargs -d'\n' xargs systemd-run --user rss-open-read.sh
