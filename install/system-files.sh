b24df42b99da21f01e4bcc2f0e2af28ffc6d1edc
Jimmi Holst Christensen <git@hejsil.anonaddy.me>
2024-05-03 23:42:15 +0200

    modprobe.d: Remove unused config

--- install/system-files.sh
+++ install/system-files.sh
@@ -4,5 +4,4 @@ cd "$(dirname "$0")/.." || exit 1
 
 sudo cp -ri 'config/pacman.conf' '/etc/pacman.conf'
 sudo cp -ri 'config/systemd-system/getty@tty1.service.d' '/etc/systemd/system/'
-sudo cp -ri config/modprobe.d/* '/etc/modprobe.d/'
 sudo cp -ri config/sysctl.d/* '/etc/sysctl.d/'
