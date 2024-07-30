000097d91c9448c99eef75767d2c91ebdf468365
Jimmi Holst Christensen <git@hejsil.anonaddy.me>
2024-07-30 23:53:53 +0200

    rss: Small refactor of rss-open-all-in-browser.sh

--- local/script/rss-open-all-in-browser.sh
+++ local/script/rss-open-all-in-browser.sh
@@ -2,9 +2,7 @@
 
 tab=$(printf '\t')
 rss-list unread | cut -f1,4,8 |
-    # Some links can be modified to open better
-    sd 'https://nyaa.si/download/(\w+).torrent' 'https://nyaa.si/view/$1' |
     while IFS=$tab read -r file link enclosure; do
-        printf '%s\t%s\t%s\t%s\n' "$(printf '%s\t\n' "$link" | opener)" "$file" "$link" "$enclosure"
+        printf '%s\t%s\t%s\t%s\n' "$(echo "$link" | opener)" "$file" "$link" "$enclosure"
     done | grep "^firefox" | cut -f2,3,4 | sort -R | head -n10 | tr '\t' '\n' |
     exec xargs -d'\n' xargs systemd-run --user rss-open-read.sh
