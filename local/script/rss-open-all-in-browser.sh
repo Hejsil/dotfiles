9abe93c1910300de8ce667bc7972423bd58be1c1
Jimmi Holst Christensen <git@hejsil.anonaddy.me>
2024-08-25 22:32:56 +0200

    opener: Do not use mpv for youtube

--- local/script/rss-open-all-in-browser.sh
+++ local/script/rss-open-all-in-browser.sh
@@ -4,5 +4,5 @@ tab=$(printf '\t')
 rss-list unread | cut -f1,4,8 |
     while IFS=$tab read -r file link enclosure; do
         printf '%s\t%s\t%s\t%s\n' "$(echo "$link" | opener)" "$file" "$link" "$enclosure"
-    done | grep "^firefox" | cut -f2,3,4 | sort -R | head -n10 | tr '\t' '\n' |
+    done | grep "^browser\s" | cut -f2,3,4 | sort -R | head -n10 | tr '\t' '\n' |
     exec xargs -d'\n' xargs systemd-run --user rss-open-read.sh
