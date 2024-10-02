14dc3fcafe6f4cd8d45b91cc60f7044f7abfb65d
Jimmi Holst Christensen <git@hejsil.anonaddy.me>
2024-10-02 17:12:20 +0200

    opener: Use xdg-open instead

--- local/script/rss-open-all-in-browser.sh
+++ local/script/rss-open-all-in-browser.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-tab=$(printf '\t')
-rss-list unread | cut -f1,4,8 |
-    while IFS=$tab read -r file link enclosure; do
-        printf '%s\t%s\t%s\t%s\n' "$(echo "$link" | opener)" "$file" "$link" "$enclosure"
-    done | grep "^browser\s" | cut -f2,3,4 | sort -R | head -n10 | tr '\t' '\n' |
-    exec xargs -d'\n' xargs systemd-run --user rss-open-read.sh
+rss-list unread | cut -f1,4 |
+    rg -v '^[^\t]*\t(https://(www.)?(youtube|odysee).com)' |
+    sort -R | head -n10 | tr '\t' '\n' |
+    exec nargs systemd-run --user rss-open-read.sh
--- local/script/rss-open-read.sh
+++ local/script/rss-open-read.sh
@@ -3,9 +3,8 @@
 input=$(mktemp -t rss-open-read.XXXXXXXXXX)
 for arg in "$@"; do
     printf '%s\n' "$arg"
-done | paste - - - >"$input"
+done | paste - - >"$input"
 
-cut -f2 "$input" | xargs -d '\n' open ||
-    cut -f3 "$input" | xargs -d '\n' open || exit 1
-cut -f1 "$input" | xargs -d '\n' -I{} mv {} "$HOME/.local/share/rss/read"
+cut -f2 "$input" | nargs systemd-run --user xdg-open
+cut -f1 "$input" | nargs -I{} mv {} "$HOME/.local/share/rss/read"
 exec rm "$input"
