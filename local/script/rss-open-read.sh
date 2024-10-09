f690ca360e23a3d29e28be4de244ab9de2471a6b
Jimmi Holst Christensen <git@hejsil.anonaddy.me>
2024-10-09 17:56:31 +0200

    rss: Don't open browser when there is nothing to read

--- local/script/rss-open-read.sh
+++ local/script/rss-open-read.sh
@@ -5,6 +5,6 @@ for arg in "$@"; do
     printf '%s\n' "$arg"
 done | paste - - >"$input"
 
-cut -f2 "$input" | nargs systemd-run --user browser
+cut -f2 "$input" | nargs -r systemd-run --user browser
 cut -f1 "$input" | nargs -I{} mv {} "$HOME/.local/share/rss/read"
 exec rm "$input"
