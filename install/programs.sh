aa42faeb8906d1f1515e5a19cb5251b24c5443a5
Jimmi Holst Christensen <git@hejsil.anonaddy.me>
2024-10-16 18:42:14 +0200

    install: Less system files

--- install/programs.sh
+++ install/programs.sh
@@ -5,6 +5,13 @@ sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
+if ! grep '\[chaotic-aur\]' /etc/pacman.conf; then
+    {
+        echo '[chaotic-aur]'
+        echo 'Include = /etc/pacman.d/chaotic-mirrorlist'
+    } | sudo tee -a /etc/pacman.conf
+fi
+
 sudo pacman -Syu
 
 (
--- install/system-files.sh
+++ install/system-files.sh
@@ -1,5 +1,3 @@
 #!/bin/sh -e
 
-sudo cp -ri "$HOME/.config/pacman.conf" '/etc/pacman.conf'
 sudo cp -ri "$HOME/.config/systemd-system/getty@tty1.service.d" '/etc/systemd/system/'
-sudo cp -ri "$HOME"/config/sysctl.d/* '/etc/sysctl.d/'
