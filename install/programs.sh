ab058730a8ad997af698f35de39ca94c5e1fdb36
Jimmi Holst Christensen <git@hejsil.anonaddy.me>
2024-10-16 18:57:15 +0200

    install: noconfirm chaotic-aur

--- install/programs.sh
+++ install/programs.sh
@@ -2,8 +2,8 @@
 
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
 if ! grep '\[chaotic-aur\]' /etc/pacman.conf; then
     {
