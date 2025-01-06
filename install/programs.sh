26bd95584045bb5610a4928a4e2f0d64f91440b4
Jimmi Holst Christensen <git@hejsil.anonaddy.me>
2025-01-06 17:30:12 +0100

    install: Cleanup

--- install/programs.sh
+++ install/programs.sh
@@ -5,6 +5,8 @@ sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
 sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
+sudo sed -i 's/^#Color$/Color/' /etc/pacman.conf
+
 if ! grep '\[chaotic-aur\]' /etc/pacman.conf; then
     {
         echo '[chaotic-aur]'
@@ -14,6 +16,7 @@ fi
 
 sudo pacman -Syu
 sudo pacman -S --noconfirm yay
+
 yay -S --noconfirm --asexplicit - <"$HOME/.config/essential-programs"
 yay -S --noconfirm --asexplicit - <"$HOME/.config/installed-programs" || true
 
