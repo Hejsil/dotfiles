5388b2aedd0597fdd16a8160b78864c9ab1622c6
Jimmi Holst Christensen <git@hejsil.anonaddy.me>
2025-01-10 22:17:21 +0100

    mpv: New chapterskip script

--- config/mpv/scripts/chapterskip.lua
+++ config/mpv/scripts/chapterskip.lua
@@ -1,86 +1,40 @@
--- chapterskip.lua
+-- Inspired by https://github.com/po5/chapterskip/blob/master/chapterskip.lua
+-- The above script was not robust enough. I often had to edit the `chapterskip.conf` when watching
+-- something new because `Intro` could either mean the opening music video, or just the first part
+-- of an episode.
 --
--- Ain't Nobody Got Time for That
+-- This script instead adds a `chapter-skip` keybind. When performed, the chapter is skipped and
+-- the title is recorded. From then on, any chapter with that title will be skipped (once).
 --
--- This script skips chapters based on their title.
+-- TODO: A `chapter-unskip` keybind might also be useful
 
-local categories = {
-    prologue = "^Prologue/Prologue$",
-    intro = "^Intro/Intro$",
-    opening = "^OP/ OP$/^Opening/Opening$",
-    ending = "^ED/ ED$/^Ending/^Credits/^Endcard",
-    preview = "Preview$/^Next/^Extra/^PV/^Hunterpedia/^Greed Island Tutorial/^Outro/^SP/^Promo/^Additional T[iI]me/^Additonal Time"
-}
-
-local options = {
-    enabled = true,
-    skip_once = true,
-    categories = "",
-    skip = ""
-}
-
-mp.options = require "mp.options"
-
-function matches(i, title)
-    for category in string.gmatch(options.skip, " *([^;]*[^; ]) *") do
-        if categories[category:lower()] then
-            if string.find(category:lower(), "^idx%-") == nil then
-                if title then
-                    for pattern in string.gmatch(categories[category:lower()], "([^/]+)") do
-                        if string.match(title, pattern) then
-                            return true
-                        end
-                    end
-                end
-            else
-                for pattern in string.gmatch(categories[category:lower()], "([^/]+)") do
-                    if tonumber(pattern) == i then
-                        return true
-                    end
-                end
-            end
-        end
-    end
-end
-
-local skipped = {}
-local parsed = {}
+local chapters_skipped = {}
+local chapters_to_skip = {}
 
 function chapterskip(_, current)
-    mp.options.read_options(options, "chapterskip")
-    if not options.enabled then return end
-    for category in string.gmatch(options.categories, "([^;]+)") do
-        name, patterns = string.match(category, " *([^+>]*[^+> ]) *[+>](.*)")
-        if name then
-            categories[name:lower()] = patterns
-        elseif not parsed[category] then
-            mp.msg.warn("Improper category definition: " .. category)
-        end
-        parsed[category] = true
-    end
     local chapters = mp.get_property_native("chapter-list")
-    local skip = false
-    for i, chapter in ipairs(chapters) do
-        if (not options.skip_once or not skipped[i]) and matches(i, chapter.title) then
-            if i == current + 1 or skip == i - 1 then
-                if skip then
-                    skipped[skip] = true
-                end
-                skip = i
-            end
-        elseif skip then
-            mp.set_property("time-pos", chapter.time)
-            skipped[skip] = true
-            return
-        end
-    end
-    if skip then
-        if mp.get_property_native("playlist-count") == mp.get_property_native("playlist-pos-1") then
-            return mp.set_property("time-pos", mp.get_property_native("duration"))
-        end
-        mp.commandv("playlist-next")
-    end
+    if current  == nil then return end
+    if chapters == nil then return end
+
+    local chapter = chapters[current + 1]
+    if chapter == nil                      then return end
+    if not chapters_to_skip[chapter.title] then return end
+    if chapters_skipped[chapter.title]     then return end
+
+    chapters_skipped[chapter.title] = true
+    mp.commandv("add", "chapter", 1)
 end
 
+mp.add_key_binding("Ctrl+s", "chapter-skip",
+    function()
+        local chapters = mp.get_property_native("chapter-list")
+        local chapter = mp.get_property_number("chapter")
+        if chapter and chapters then
+            chapters_to_skip[chapters[chapter + 1].title] = true
+            chapters_skipped[chapters[chapter + 1].title] = true
+        end
+        mp.commandv("add", "chapter", 1)
+    end)
+
 mp.observe_property("chapter", "number", chapterskip)
-mp.register_event("file-loaded", function() skipped = {} end)
+mp.register_event("file-loaded", function() chapters_skipped = {} end)
