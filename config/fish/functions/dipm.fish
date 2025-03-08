c06557a8bd8040f606f717077b6e39455edb2a41
Jimmi Holst Christensen <git@hejsil.anonaddy.me>
2025-03-08 23:23:07 +0100

    fish: Fish dipm alias

--- config/fish/functions/dipm.fish
+++ config/fish/functions/dipm.fish
@@ -1,4 +1,4 @@
-function cd --wraps=dipm --description 'alias dipm=dipm'
+function dipm --wraps=dipm --description 'alias dipm=dipm'
     command dipm $argv
     command dipm list installed | sort | cut -f1 >"$XDG_CONFIG_HOME/dipm-installed-programs"
 end
