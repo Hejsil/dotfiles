7f90119e58e2d34e297e0c68037b35127c67dada
Jimmi Holst Christensen <git@hejsil.anonaddy.me>
2025-03-20 22:56:57 +0100

    rss: Open in new tab of existing browser if possible

--- local/script/rss-open-read.sh
+++ local/script/rss-open-read.sh
@@ -5,6 +5,6 @@ for arg in "$@"; do
     printf '%s\n' "$arg"
 done | paste - - >"$input"
 
-cut -f2 "$input" | nargs -r systemd-run --user browser
+cut -f2 "$input" | sed 's/^/--new-tab\n/' | nargs -r systemd-run --user browser
 cut -f1 "$input" | nargs -I{} mv {} "$HOME/.local/share/rss/read"
 exec rm "$input"
