8436bf0eb9311f60f76d8b7f4733be29c279be0a
Jimmi Holst Christensen <git@hejsil.anonaddy.me>
2025-06-04 13:53:45 +0200

    fish: Only source tools if they actually exist

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,17 @@
 if status is-interactive
-    atuin init fish | source
-    carapace _carapace | source
-    starship init fish | source
-    zoxide init fish | source
+    if type -q atuin
+        atuin init fish | source
+    end
+    if type -q carapace
+        carapace _carapace | source
+    end
+    if type -q starship
+        starship init fish | source
+    end
+    if type -q zoxide
+        zoxide init fish | source
+    end
+    if type -q dasel
+        dasel completion fish | source
+    end
 end
