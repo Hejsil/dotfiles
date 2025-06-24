8a8c6a9402c5612f0661d08f12536995d92e9f3d
Jimmi Holst Christensen <git@hejsil.anonaddy.me>
2025-06-24 20:17:37 +0200

    pkgs: Sort packages from dipm

--- config/fish/functions/dipm.fish
+++ config/fish/functions/dipm.fish
@@ -2,5 +2,5 @@ function dipm --wraps=dipm --description 'alias dipm=dipm'
     command dipm $argv
 
     mkdir -p "$XDG_CONFIG_HOME/pkgs"
-    command dipm list installed | cut -f1 >"$XDG_CONFIG_HOME/pkgs/dipm"
+    command dipm list installed | cut -f1 | sort >"$XDG_CONFIG_HOME/pkgs/dipm"
 end
