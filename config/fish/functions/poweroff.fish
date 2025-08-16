a9ff70f69d74f557391f8133ebc06dec1d321b80
Jimmi Holst Christensen <git@hejsil.anonaddy.me>
2025-08-16 09:56:34 +0200

    fish: Remove poweroff and reboot aliases

--- config/fish/functions/poweroff.fish
+++
@@ -1,5 +0,0 @@
-function poweroff --wraps=poweroff --description 'alias poweroff=poweroff'
-    topgrade
-    sleep 5s
-    command poweroff
-end
--- config/fish/functions/reboot.fish
+++
@@ -1,5 +0,0 @@
-function reboot --wraps=reboot --description 'alias reboot=reboot'
-    topgrade
-    sleep 5s
-    command reboot
-end
