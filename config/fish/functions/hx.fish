8b8664683221f1c05051fac9b81036dc989cafcd
Jimmi Holst Christensen <git@hejsil.anonaddy.me>
2026-02-02 13:46:52 +0100

    fish: Better handle helix alias

--- config/fish/functions/hx.fish
+++ config/fish/functions/hx.fish
@@ -1,3 +1,6 @@
-function hx --wraps='helix' --description 'alias ga=helix'
-    helix $argv
+function hx --wraps='helix' --description 'alias hx=helix'
+    if type -q helix
+        command helix $argv
+    end
+    command hx $argv
 end
